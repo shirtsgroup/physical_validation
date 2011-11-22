4563263756d619a5d7bddf5d31d9819e8294d22c
mrshirts <mrshirts@3b6020e3-42ab-0410-be60-f0e2a992d8c5>
2011-11-22 01:35:54 +0000

    Added ability to look at multidimensional harmonic oscillators.
    git-svn-id: https://simtk.org/svn/checkensemble@13 3b6020e3-42ab-0410-be60-f0e2a992d8c5

--- harmonic.py
+++ harmonic.py
@@ -25,8 +25,10 @@ parser.add_option("-i", "--nbins", dest="nbins",type = "int", default=30,
                   help="number of bins for bootstrapping [default = %default]") 
 parser.add_option("-v", "--verbose", dest="verbose", action="store_true",default=False,
                   help="more verbosity")
-parser.add_option("-N", "--number", nargs = 2, dest="N_k", type="int",default=[100000,100000],
+parser.add_option("-N", "--number of samples", nargs = 2, dest="N_k", type="int",default=[100000,100000],
                   help="number of samples from the two states, [default = %default]") 
+parser.add_option("-D", "--number of dimensions", dest="D", type="int",default=1,
+                  help="number of dimensions, [default = %default]") 
 parser.add_option("-g", "--figureprefix", dest="figname", default='harmonic',
                   help="name prefix for the figure")
 parser.add_option("-o", "--noise", dest="noise", type = 'float', default=0.0,
@@ -38,21 +40,28 @@ parser.add_option("-s", "--seed", dest="seed", type = 'int', default=None,
 (options, args) = parser.parse_args()
 
 # For a 1D harmonic oscillator, the potential is given by                                
-#   V(x;K) = (K/2) * (x-x_0)**2                                                                             
+#   V(x;K) = \sum_{i=1}^D (K/2) * (x_i-x_0,i)**2                                                                             
 # where K denotes the spring constant.                                  
 #                                                                                                   
 # The equilibrium distribution is given analytically by                                      
 #   p(x;beta,K) = sqrt[(beta K) / (2 pi)] exp[-beta K (x-x_0)**2 / 2]                                                       
 # The dimensionless free energy is therefore                                                
-#   f(beta,K) = - (1/2) * ln[ (2 pi) / (beta K) ]              
-#   f_2 - f_1 = -(1/2) (ln [ 1/ beta_2 K_2] - ln [ 1/beta_1 K_1])  
-#   f_2 - f_1 = (1/2) ln [beta_2/K_2 / beta_1/K_1]  
-#   f_2 - f_1 = (1/2) ln [K_1 T_1/ K_2 T_2]  
-#   for T_1 = 0.9, T_2 = 1.1, K_1 = K_2, df = 0.5*ln(1.1/0.9) = 0.10034
-#   for T_1 = 0.8, T_2 = 1.2, K_1 = K_2, df = 0.5*ln(1.2/0.8) = 0.2027
-#   for T_1 = 0.5, T_2 = 1.5, K_1 = K_2, df = 0.5*ln(1.5/0.5) = 0.5493
+#   f(beta,K) = - (D/2) * ln[ (2 pi) / (beta K) ]              
+#   f_2 - f_1 = -(D/2) (ln [ 1/ beta_2 K_2] - ln [ 1/beta_1 K_1])  
+#   f_2 - f_1 = (D/2) ln [beta_2/K_2 / beta_1/K_1]  
+#   f_2 - f_1 = (D/2) ln [K_1 T_1/ K_2 T_2]  
+#   for T_1 = 0.9, T_2 = 1.1, K_1 = K_2, df = 0.5*ln(1.1/0.9) = 0.10034D
+#   for T_1 = 0.8, T_2 = 1.2, K_1 = K_2, df = 0.5*ln(1.2/0.8) = 0.2027D
+#   for T_1 = 0.5, T_2 = 1.5, K_1 = K_2, df = 0.5*ln(1.5/0.5) = 0.5493D
 #   analytical variance = depends on the method.  So won't try 
-
+#
+#   average energy is -\pd{ln Q}{beta}
+#   ln Q  = (D/2) * ln[ (2 pi) / (beta K) ]             
+#   -pd{-(D/2) * ln[ (2 pi) / (beta K)}{\beta} = 
+#   D/2 pd{ln[(beta K)}{\beta} = 
+#   D/2 pd{ln[(beta)}{\beta} = 
+#   D/2 beta^{-1} 
+#
 title='harmonic oscillators'
 if (options.nreps > 0):
     reptype = 'independent'
@@ -68,6 +77,7 @@ if (options.seed):
     print "setting random number seed for generating samples as %d" % (options.seed)
 
 kB = 1.0
+D = options.D
 K_k = options.K*numpy.array([1,1])
 T_k = numpy.array(options.T_k) #T_k = numpy.array([0.5,1.5])  # temperatures
 noise = options.noise  #Random noise
@@ -84,7 +94,7 @@ K = len(beta_k)
 x_kn = numpy.zeros([K,N_max], float) # x_kn[k,n] is the coordinate x of independent snapshot n of simulation k   
 U_kn = numpy.zeros([K,N_max], float) # x_kn[k,n] is the energy of the sample at x_kn[k,n]
 
-df = 0.5*numpy.log(beta_k[1]/beta_k[0]) # analytical result
+df = D*0.5*numpy.log(beta_k[1]/beta_k[0]) # analytical result
 print "Analytical df = %.8f" % (df)
 
 print "Now sampling %d sets of data . . . could also take a bit" % (nreps)
@@ -94,14 +104,15 @@ reps = []
 for n in range(nreps):
     if (n%10 == 0):
         print "Finished generating %d sets . . ." % (n)
-
-    for k in range(0,K):
-        # generate N_k[k] independent samples with spring constant K_k[k]                   
-        x_kn[k,0:N_k[k]] = numpy.random.normal(O_k[k], sigma_k[k], N_k[k])
-        # compute potential energy of all samples in all potentials 
-        U_kn[k,:] = (K_k[k]*(1+noise)/2.0) * (x_kn[k,:]-O_k[k])**2 
+    U_kn[:,:] = 0
+    for k in range(K):
+        for d in range(D):
+            # generate N_k[k] independent samples with spring constant K_k[k]                   
+            x_kn[k,0:N_k[k]] = numpy.random.normal(O_k[k], sigma_k[k], N_k[k])
+            # compute potential energy of all samples in all potentials 
+            U_kn[k,:] += (K_k[k]*(1+noise)/2.0) * (x_kn[k,:]-O_k[k])**2 
         if (noise > 0):
-            U_kn[k,:] += numpy.abs(noise*numpy.random.normal(O_k[k],1,[N_k[k]])) # add noise to the potential
+            U_kn[k,:] += D*numpy.abs(noise*numpy.random.normal(O_k[k],1,[N_k[k]])) # add noise to the potential
 
     addrep = [U_kn.copy()]    
     reps.append(addrep)
