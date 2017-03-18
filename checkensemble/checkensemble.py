448b33e6ec749edb02433e2bf5e456dac0937303
Michael Shirts <mrshirts@gmail.com>
2017-03-18 17:49:47 -0600

    Reorganizing the files for better packaging.

--- checkensemble/checkensemble.py
+++ checkensemble/checkensemble.py
@@ -5,7 +5,7 @@
 #
 # Written by Michael R. Shirts <mrshirts@gmail.com>.
 #
-# Copyright (c) 2012 The University of Virginia. All Rights Reserved.
+# Copyright (c) 2012 The University of Virginia, (c) 2017 University of Colorado Boulder.  All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or modify it under the terms of
 # the GNU General Public License as published by the Free Software Foundation; either version 2
