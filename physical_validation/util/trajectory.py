193036d5577e99bc5923978c6ca7d110d3cec77b
Pascal Merz <pascal.merz@colorado.edu>
2017-11-27 18:27:45 -0700

    Fixes output in py2.7

--- physical_validation/util/trajectory.py
+++ physical_validation/util/trajectory.py
@@ -26,6 +26,8 @@
 #                                                                         #
 ###########################################################################
 
+from __future__ import division
+
 import numpy as np
 from scipy import stats
 
