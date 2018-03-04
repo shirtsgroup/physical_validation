612717a158aadfd3959e88af498590b8a8c314a4
Pascal Merz <pascal.merz@colorado.edu>
2018-03-04 16:14:35 -0700

    Changes to setup.py for pypi upload. Partially solves #2

--- setup.py
+++ setup.py
@@ -1,49 +1,63 @@
 r"""
 `physical_validation` is a package aimed at testing results obtained
-by molecular dynamics simulations for their physical validity.
+by molecular simulations for their physical validity.
 
 shirtsgroup.github.io/physical-validation
 """
 from __future__ import print_function
 
 from setuptools import setup, find_packages
+from os import path
 
 #####################################
-VERSION = "0.1.0"
-ISRELEASED = False
-if ISRELEASED:
-    __version__ = VERSION
-else:
-    __version__ = VERSION + 'a1'
+VERSION = "1.0.0rc3"
+__version__ = VERSION
+
 #####################################
 
-with open('requirements.txt') as reqs_file:
-    reqs = [line.strip() for line in reqs_file]
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='physical_validation',
     version=__version__,
-    description=__doc__,
-    author='Michael R. Shirts, Pascal T. Merz',
-    author_email='michael.shirts@colorado.edu, pascal.merz@colorado.edu',
-    url='https://github.com/shirtsgroup/physical-validation',
-    packages=find_packages(),
-    package_dir={'physical_validation': 'physical_validation'},
-    install_requires=reqs,
-    license="LGPLv2.1",
-    zip_safe=False,
-    keywords='physical_validation',
+    description='Physical validation of molecular simulation results',
+    long_description='\n' + long_description,
+    url='https://physical-validation.readthedocs.io',
+    author='Pascal T. Merz and Michael R. Shirts',
+    author_email='pascal.merz@colorado.edu, michael.shirts@colorado.edu',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: LGPLv2.1 License',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
         'Natural Language :: English',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Environment :: Console',
+        'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Chemistry',
-        'Topic :: Scientific/Engineering :: Molecular-Simulation',
         'Topic :: Software Development :: Libraries :: Python Modules'
         ],
+    keywords='physical-validation, molecular-simulation, molecular-dynamics, molecular-mechanics',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pymbar'
+        ],
+    package_dir={'physical_validation': 'physical_validation'},
+    project_urls={
+        'Bug Reports': 'https://github.com/shirtsgroup/physical_validation/issues',
+        'Documentation': 'https://physical-validation.readthedocs.io',
+        'Source': 'https://github.com/shirtsgroup/physical_validation',
+    },
+    license="LGPLv2.1",
 )
