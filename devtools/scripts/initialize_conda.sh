357c99b9fc905a3e0ff321ba29031683d63f8675
Matthew W. Thompson <mattwthompson@protonmail.com>
2021-01-22 14:07:06 -0600

    Switch to miniconda-setup

--- devtools/scripts/initialize_conda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-case $OPERATING_SYSTEM in
-    windows*)
-        eval "$(${CONDA}/condabin/conda.bat shell.bash hook)";;
-    *)
-        eval "$(${CONDA}/condabin/conda shell.bash hook)";;
-esac
