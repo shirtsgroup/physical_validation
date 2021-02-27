28e28f70444b67717f69d34df5cb34d9a95c243c
Pascal Merz <pascal.merz@me.com>
2021-02-27 14:02:58 -0700

    Update copyright headers

--- physical_validation/tests/test_physical_validation.py
+++ physical_validation/tests/test_physical_validation.py
@@ -1,3 +1,15 @@
+###########################################################################
+#                                                                         #
+#    physical_validation,                                                 #
+#    a python package to test the physical validity of MD results         #
+#                                                                         #
+#    Written by Michael R. Shirts <michael.shirts@colorado.edu>           #
+#               Pascal T. Merz <pascal.merz@colorado.edu>                 #
+#                                                                         #
+#    Copyright (c) 2017-2021 University of Colorado Boulder               #
+#              (c) 2012      The University of Virginia                   #
+#                                                                         #
+###########################################################################
 """
 Unit and regression test for the physical_validation package.
 """
