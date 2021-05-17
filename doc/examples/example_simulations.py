013630d6bc12bc3b703c44c766df819a1d601d1e
Pascal Merz <pascal.merz@me.com>
2021-05-16 23:30:06 -0600

    Transfer ensemble check example to Jupyter notebook

--- doc/examples/example_simulations.py
+++ doc/examples/example_simulations.py
@@ -20,4 +20,36 @@ def get(simulation_identifier: str) -> Dict[str, np.ndarray]:
 
         return water_NVT_298_berendsen.get()
 
+    if (
+        simulation_identifier
+        == "900 water molecules, NVT at 308K with v-rescale thermostat"
+    ):
+        from simulation_results import water_NVT_308_vrescale
+
+        return water_NVT_308_vrescale.get()
+
+    if (
+        simulation_identifier
+        == "900 water molecules, NVT at 308K with Berendsen thermostat"
+    ):
+        from simulation_results import water_NVT_308_berendsen
+
+        return water_NVT_308_berendsen.get()
+
+    if (
+        simulation_identifier
+        == "900 water molecules, NPT at 298K and 1bar, using v-rescale and Parrinello-Rahman"
+    ):
+        from simulation_results import water_NPT_298_1
+
+        return water_NPT_298_1.get()
+
+    if (
+        simulation_identifier
+        == "900 water molecules, NPT at 308K and 101bar, using v-rescale and Parrinello-Rahman"
+    ):
+        from simulation_results import water_NPT_308_101
+
+        return water_NPT_308_101.get()
+
     raise KeyError(f"Unknown simulation: {simulation_identifier}")
