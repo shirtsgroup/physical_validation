74ff6b352629d9dd222852026e7e9602e1d587f4
Pascal Merz <pascal.merz@me.com>
2021-11-11 18:04:31 +0100

    Bump version number to reflect addition of conda installation instructions

--- doc/conf.py
+++ doc/conf.py
@@ -67,7 +67,7 @@ author = 'Pascal T. Merz, Michael R. Shirts'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = "1.0.0"
+release = "1.0.1"
 # The short X.Y version.
 version = ".".join(release.split(".")[:2])
 
